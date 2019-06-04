c2e187e401f962f210ccb8706dbdbe6e49648b1f
Vladimir Kurbatov <warkb@yandex.ru>
2019-06-04 15:20:52 +0400

    Сделал лейблы для инпутов

--- static/app/log-page/log-page.component.js
+++ static/app/log-page/log-page.component.js
@@ -1,7 +1,29 @@
 'use strict'
 
 angular.
-	module('farmNetApp').
-	component('logPage', {
-		templateUrl: '/static/app/log-page/log-page.template.html',
+module('farmNetApp').
+component('logPage', {
+	templateUrl: '/static/app/log-page/log-page.template.html',
+	controller: function () {
+		let loginCardElement = $('#login-card');
+		let top = ($(window).height() - loginCardElement.height()) / 2.5;
+		let left = ($(window).width() - loginCardElement.width()) / 2;
+		loginCardElement.offset(
+			{top:top, left:left}
+			);
+			// скрипты для подсказок
+			$('input').focus(function(){
+				$(this).parents('.form-group').addClass('focused');
+			});
+
+			$('input').blur(function(){
+				var inputValue = $(this).val();
+				if ( inputValue == "" ) {
+					$(this).removeClass('filled');
+					$(this).parents('.form-group').removeClass('focused');  
+				} else {
+					$(this).addClass('filled');
+				}
+			})  
+		}
 	});
