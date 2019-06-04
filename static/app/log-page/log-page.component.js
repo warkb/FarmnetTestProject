c05eafd90485e04021ad14eb83168ff5eadba249
Vladimir Kurbatov <warkb@yandex.ru>
2019-06-04 16:08:33 +0400

    Сделал анимацию для логина и переход по ссылке

--- static/app/log-page/log-page.component.js
+++ static/app/log-page/log-page.component.js
@@ -4,26 +4,41 @@ angular.
 module('farmNetApp').
 component('logPage', {
 	templateUrl: '/static/app/log-page/log-page.template.html',
-	controller: function () {
-		let loginCardElement = $('#login-card');
-		let top = ($(window).height() - loginCardElement.height()) / 2.5;
-		let left = ($(window).width() - loginCardElement.width()) / 2;
-		loginCardElement.offset(
-			{top:top, left:left}
-			);
-			// скрипты для подсказок
-			$('input').focus(function(){
-				$(this).parents('.form-group').addClass('focused');
-			});
-
-			$('input').blur(function(){
-				var inputValue = $(this).val();
-				if ( inputValue == "" ) {
-					$(this).removeClass('filled');
-					$(this).parents('.form-group').removeClass('focused');  
-				} else {
-					$(this).addClass('filled');
+	controller: ['$location', function LogPageController($location) {
+			var self = this;
+			self.step = 0;
+			self.init_tips = function () {
+				// инициализирует подсказки
+				$('input').focus(function(){
+					$(this).parents('.form-group').addClass('focused');
+				});
+	
+				$('input').blur(function(){
+					var inputValue = $(this).val();
+					if ( inputValue == "" ) {
+						$(this).removeClass('filled');
+						$(this).parents('.form-group').removeClass('focused');  
+					} else {
+						$(this).addClass('filled');
+					}
+				});
+			};
+			self.next_click = function () {
+				// срабатывает при клике на кнопку Далее
+				if (self.step === 0) {
+					self.step += 1;
+					$('#inputs').animate({left: '-270px'}, 300);
 				}
-			})  
-		}
-	});
+				else {
+					$location.path('/persons');
+				}
+			}
+			let loginCardElement = $('#login-card');
+			let top = ($(window).height() - loginCardElement.height()) / 2.5;
+			let left = ($(window).width() - loginCardElement.width()) / 2;
+			loginCardElement.offset(
+				{top:top, left:left}
+				);
+			self.init_tips();
+		}]
+});
